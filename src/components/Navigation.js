27d7a1805d2ff041750149e0d59de27cdcc02bc3
github username <github email address>
2019-09-29 19:24:16 -0400

    added Nav Bar component with Bootstrap and Buttons to login and sign up

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 
 class Navigation extends React.Component {
   
   render(){
-    return <div>
+    return <Navbar variant="dark">
 
-      This is the Nav Bar
+      <Navbar.Brand href="#home">🌲🌰 Seeds Store 🌰🌲</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+        &nbsp;&nbsp;&nbsp;
+        <Nav>
+          <ButtonToolbar>
+            <Button variant="light">Sign Up</Button>
+            &nbsp;&nbsp;&nbsp;
+            <Button variant="light">Login</Button>
+          </ButtonToolbar>
+        </Nav>
 
-    </div>
+     </Navbar>
 
   }
 }
